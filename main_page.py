a10e7c2de06e77d03109f4434c5a846aa44a4308
HaoYu <yuha1k@novartis.net>
2024-09-07 13:47:30 -0400

    refine content

--- main_page.py
+++ main_page.py
@@ -1,18 +1,20 @@
 import streamlit as st
 
-
-st.markdown("# Internship at Novartis Institutes for BioMedical Research (NIBR)")
+st.markdown("# Internship at Discovery Sciences, Data Science Team of Novartis Institutes for BioMedical Research (NIBR)")
 st.markdown("##### June 3rd - August 9th")
 st.markdown("##### Author: Hao Yu")
 st.markdown("##### Affliation: Electrical and Computer Engineering, Boston University")
 st.markdown("##### Contact: imhaoyu@bu.edu")
-st.markdown("## Introduction")
-st.write("Modelling the interactions between protein and small molecule ligands is crucial for structure-based drug design. While significant advances have been made in predicting apo-protein structures from sequence with deep learning, obtaining co-folded protein-ligand complex structures would be one step forward to AI-based drug discovery. Current available models achieving the goal of protein-ligand co-folding from sequence information include Umol and RoseTTAFold All-Atom. In their respective publications, the performance of these models were tested on public available datasets, which included endogenous ligands and don’t fully reflect drug-discovery applications. This caveat motivates us to benchmark these methods on ligand-protein co-crystallized structures from the Novartis internal Proasis database to assess their performance and determine suitability for internal drug discovery programs. ")
-st.markdown("## Details")
+st.markdown("## Background")
+st.write(open("./txt/introduction.txt").read())
+st.markdown("## Project Overview")
+st.write()
 st.markdown("## Summary")
+st.image(["./figures/umol-plddt.png","./figures/rfaa-plddt.png"])
 st.markdown("### References")
-st.write("1. Bryant, P. et al. Structure prediction of protein-ligand complexes from sequence information with Umol. Nat. Commun. 2024")
-st.write("2. Krishna, R. et al. Generalized biomolecular modeling and design with RoseTTAFold All-Atom. Science. 2024")
+st.write("1. Jumper, J. et al. Highly accurate protein structure prediction with AlphaFold. Nature. 2021")
+st.write("2. Bryant, P. et al. Structure prediction of protein-ligand complexes from sequence information with Umol. Nat. Commun. 2024")
+st.write("3. Krishna, R. et al. Generalized biomolecular modeling and design with RoseTTAFold All-Atom. Science. 2024")
 st.markdown("### Acknowledgement")
 st.write("This internship project is supported by the Professional Development & Postdoctoral Affairs National Science Foundation Innovations in Graduate Education from Boston University. \n I would like to thank supervision from my mentors Jian Fang, Dave Barkan, Lingling Shen, Roman Sloutsky, Christian Schleberger and Peter Kutchukian.")
 
--- pages/takeaways.py
+++ pages/takeaways.py
@@ -6,6 +6,7 @@ st.markdown("- Collaborative brainstorm for plans")
 st.markdown("- Keep updated with frontier science")
 st.markdown("- Working on real world problem")
 
+
 st.markdown("## Relavence to PhD research")
 st.markdown("- Gained the sense of frontier of AI for Biology")
 st.markdown("- Gained troubleshooting skillset in distributed system")
