1ab091d3e15725631e1b80a43427c886bbc7923f
GravityPHY <36836179+GravityPHY@users.noreply.github.com>
2024-10-04 00:33:39 -0400

    add prompt

--- pages/demo_page.py
+++ pages/demo_page.py
@@ -122,49 +122,6 @@ if hl_ligand:
     col3.write("Ligand")
     col3.dataframe(dataframes.get_resi_bfactor(temp_file_path, resi_name=["UNK","LIG","LG1"]))
 
-if generate_text:
-    with st.chat_message("user"):
-        st.write("Hello 👋") 
-
-
-
-with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
-    if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
-    else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-
-    # Refactored from https://github.com/a16z-infra/llama2-chatbot
-    st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B', 'Llama2-70B'], key='selected_model')
-    if selected_model == 'Llama2-7B':
-        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
-    elif selected_model == 'Llama2-13B':
-        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    else:
-        llm = 'replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48'
-    
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
-    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
-    
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
-os.environ['REPLICATE_API_TOKEN'] = replicate_api
-
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-
-# Display or clear chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
 
 def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
@@ -178,19 +135,37 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run(llm, 
+    output = replicate.run(llm,
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
                                   "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
     return output
 
-# User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
+st.title('🦙💬 How to understand prediction confidence?')
+if 'REPLICATE_API_TOKEN' in st.secrets:
+    st.success('API key already provided!', icon='✅')
+    replicate_api = st.secrets['REPLICATE_API_TOKEN']
+else:
+    replicate_api = st.text_input('Enter Replicate API token:', type='password')
+    if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
+        st.warning('Please enter your credentials!', icon='⚠️')
+    else:
+        st.success('Proceed to entering your prompt message!', icon='👉')
+#selected_model == 'Llama2-7B'
+llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
 
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
+temperature = 0.1  # st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+top_p = 0.9  # st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+max_length = 512  # st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
+
+os.environ['REPLICATE_API_TOKEN'] = replicate_api
+
+
+if generate_text:
+    ligand_confidence=dataframes.get_resi_bfactor(temp_file_path, resi_name=["UNK","LIG","LG1"])
+    prompt=f"Can you help me answer the question with the following information? From my project, I found that if the prediction confidence is above 80, than there is 80% percent chance the complex prediction is accurate; " \
+           f"if the prediction confidence is below 80 but above 60, there is 50% chance the prediction is accurate. " \
+           f"In the other cases, there is 10% chance the prediction is accurate." \
+           f"Now the prediction confidence is {ligand_confidence['Prediction Confidence']}, tell me how accurate this prediction is? Just tell me the result, no need to show the analyzing steps"
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = generate_llama2_response(prompt)
@@ -202,3 +177,38 @@ if st.session_state.messages[-1]["role"] != "assistant":
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
+
+
+
+
+
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+# Display or clear chat messages
+#for message in st.session_state.messages:
+#    with st.chat_message(message["role"]):
+#        st.write(message["content"])
+
+#
+# User-provided prompt
+#if prompt := st.chat_input(disabled=not replicate_api):
+#    st.session_state.messages.append({"role": "user", "content": prompt})
+#    with st.chat_message("user"):
+#        st.write(prompt)
+
+## Generate a new response if last message is not from assistant
+#if st.session_state.messages[-1]["role"] != "assistant":
+#    with st.chat_message("assistant"):
+#        with st.spinner("Thinking..."):
+#            response = generate_llama2_response(prompt)
+#            placeholder = st.empty()
+#            full_response = ''
+#            for item in response:
+#                full_response += item
+#                placeholder.markdown(full_response)
+#            placeholder.markdown(full_response)
+#    message = {"role": "assistant", "content": full_response}
+#    st.session_state.messages.append(message)
